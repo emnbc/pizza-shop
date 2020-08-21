8089c718fb4aef3ae125088fa2a3c706482fe029
Evgeny Tupikov <bwinter@yandex.ru>
2020-08-21 12:19:53 +0500

    Change backend port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(3020, '0.0.0.0');
 
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
